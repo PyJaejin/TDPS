02a10540552cafa78f9af7f60df62c15b3c5a4bd
Jo-Hyuna <whgusk97@naver.com>
2019-05-21 23:11:32 +0900

    서버 실행, main 화면 생성

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route(‘/’)
-def index():
+@app.route('/test', methods=['GET'])
+def tests():
     return 'embadded test!!'
 
-if __name__ == ‘__main__’ :
+@app.route('/')
+def main():
+    return render_template("main.html")
+
+if __name__ == '__main__' :
     app.run(host='localhost', port=8080)
