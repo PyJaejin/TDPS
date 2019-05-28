08d827c76779049948f4a93a21c936848352cfa3
Jo-Hyuna <whgusk97@naver.com>
2019-05-28 18:10:13 +0900

    ajax로 DB에 저장되는 실시간 온/습도출력

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from src.DB_Controller import DB_Controller
+import json
+from datetime import datetime
 
 app = Flask(__name__)
+db_controller = DB_Controller()
 
 @app.route('/test', methods=['GET'])
 def tests():
@@ -9,6 +13,12 @@ def tests():
 @app.route('/')
 def main():
     return render_template("main.html")
+
+@app.route('/select')
+def h_select():
+    result = db_controller.get_setting()
+    print(result)
+    return json.dumps(result)
 
 if __name__ == '__main__' :
     app.run(host='localhost', port=8080)
--- src/DB_Controller.py
+++ src/DB_Controller.py
@@ -15,7 +15,7 @@ class DB_Controller():
     # setting 테이블의 모든 행을 읽어서 json으로 변환하여 리턴해주는 함수
     def get_setting(self):
 
-        node_id_list = [911] #~23센서 값
+        node_id_list = [911,912] #~23센서 값
 
         db = pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd, db=self.db,
                              charset='utf8', autocommit=True)
@@ -26,9 +26,9 @@ class DB_Controller():
             cursor.execute(sql)
             data = cursor.fetchone()
             print(data)
-            query_to_dict = ast.literal_eval("{'"+str(node_id_list[i])+"':[{'temperature':"+str(data[2])+",' humidity':"+str(data[3])+"}]}")
+            query_to_dict = ast.literal_eval("{'"+str(node_id_list[i])+"_temperature':"+str(data[2])+",'"+str(node_id_list[i])+"_humidity':"+str(data[3])+"}")
+            print(query_to_dict)
             self.setting_json.update(query_to_dict)
-
 
 
         while True:
